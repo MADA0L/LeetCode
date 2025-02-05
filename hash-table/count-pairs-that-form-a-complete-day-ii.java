be3bd669c708fb9ea5987b2f8775d6af314e8df8
MADA0L <mada0s3c@gmail.com>
2025-02-05 21:45:33 +0800

    LC-count-pairs-that-form-a-complete-day-ii [Runtime: N/A; Memory: N/A]

---
+++ hash-table/count-pairs-that-form-a-complete-day-ii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public long countCompleteDayPairs(int[] hours) {
+        int ans = 0;
+        if (hours.length == 0 ){
+            return 0;
+        }
+        int[] remainders = new int[24];
+        for (int h : hours){
+            if (h % 24 == 0){
+                ans += remainders[0];
+            } else {
+                ans +=  remainders[24 - h % 24];
+            }
+            remainders[h % 24]++;
+        }
+        return ans;
+    }
+}
