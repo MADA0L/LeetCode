764ab3133ee21b442fe9b951807dd788fb2015ad
MADA0L <mada0s3c@gmail.com>
2025-02-11 21:53:55 +0800

    LC-best-sightseeing-pair [Runtime: 4 ms; Memory: 53.2 MB]

--- array/best-sightseeing-pair.java
+++ array/best-sightseeing-pair.java
@@ -2,7 +2,7 @@ class Solution {
     public int maxScoreSightseeingPair(int[] values) {
         int ans = 0;
         int maxPrev = values[0] + 0;
-        for (int i =0 ; i< values.length; i++){
+        for (int i = 1 ; i< values.length; i++){
             ans = Math.max(ans, values[i]-i+maxPrev);
             maxPrev = Math.max(maxPrev, values[i]+i);
         }
