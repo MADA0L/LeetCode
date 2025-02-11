bb948304c7d66704655808697feb65982dc29263
MADA0L <mada0s3c@gmail.com>
2025-02-11 22:23:01 +0800

    LC-count-nice-pairs-in-an-array [Runtime: N/A; Memory: N/A]

---
+++ math/count-nice-pairs-in-an-array.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int countNicePairs(int[] nums) {
+        int ans = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i< nums.length ; i++){
+            int target = nums[i]-rev(nums[i]);
+            if(map.containsKey(target)){
+                ans = ans + map.get(target);
+            }
+            map.put(target,map.getOrDefault(target, 0) + 1);
+        }
+
+        return ans;
+    }
+
+    public static int rev (int num){
+        ArrayList<Integer> numList = new ArrayList<>();
+        int revNum = 0;
+        while (num >= 10){
+            numList.add(num % 10);
+            num = num / 10;
+        }
+        numList.add(num);
+        int size = numList.size();
+        for (int i = 0; i < numList.size(); i++){
+            revNum = revNum + (int) (numList.get(i) * Math.pow(10, size-1));
+            size--;
+        }
+        return revNum;
+    }
+}
