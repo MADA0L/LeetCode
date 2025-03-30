86cbaea25ae6bbce2d22634fbd4af85f86d0164c
MADA0L <mada0s3c@gmail.com>
2025-03-30 18:37:08 +0800

    LC-subarray-product-less-than-k [Runtime: N/A; Memory: N/A]

---
+++ array/subarray-product-less-than-k.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        int left = 0;
+        int ans = 0;
+        int product = 1;
+
+        for (int right = 0; right < nums.length; right++){
+            product *= nums[right];
+            if (product >= k){
+                product /= nums[left];
+                left += 1;
+                ans += right - left + 1;
+            } else {
+                ans += right - left + 1;
+            }
+        }
+    return ans;
+    }
+}
